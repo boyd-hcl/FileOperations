41668c5df56ad10ea14bd5b111e907503945f5e4
Al <andrewboyd@Andrews-Air.attlocal.net>
2021-02-09 22:32:39 -0600

    Finishing functionality for file operations

--- FileOperations.java
+++ FileOperations.java
@@ -20,19 +20,24 @@ public class FileOperations{
     public void Append(){
         setFile("append to.");
         try{
+            System.out.println("These are the current contents of the file.");
             BufferedReader br = new BufferedReader(new FileReader(file));
             String output = br.readLine();
             while(output!=null){
                 System.out.println(output);
             }
             br.close();
+            System.out.println("Please input the line you would like to append to the file:");
+            String input = System.console().readLine();
+            br.close();
         }
         catch(Exception e){
-            System.out.println("File is currently in use bya nother application. Please close the extrenal application and try again.")
+            System.out.println("File is currently in use by a nother application. Please close the extrenal application and try again.")
         }
         System.out.println("Please input the line to append");
         try{
-            
+            BufferedWriter bw = new BufferedWriter(new FileWriter(file));
+            bw.write(System.console().readLine());
         }
         catch(Exception e){
             
@@ -49,7 +54,7 @@ public class FileOperations{
             br.close();
         }
         catch(Exception e){
-            System.out.println("File is currently in use bya nother application. Please close the extrenal application and try again.")
+            System.out.println("File is currently in use by another application. Please close the extrenal application and try again.")
         }
     }
     public void Write(){
